92469904491d55dbbaa34c1f1f5f86e21da1b466
DashmanGC <ulricvonmesnar@gmail.com>
2014-11-25 14:51:05 +0100

    Version 1.2
    Initial release

---
+++ README.md
@@ -0,0 +1,37 @@
+SRW GC Battle Script Editor by Dashman
+------------------------------------------
+
+USING THIS
+-----------
+
+Using this program is pretty straightforward. First of all, make sure you have Java installed in your computer.
+
+To launch the program, simply double-click on it.
+
+This application allows you to edit the contents of BIN files extracted from bpilot.pak (using the BIN Splitter). Go to File -> Open... and browse for the file you want to edit.
+
+Once you load the file, you'll be able to navigate through the "conversations" inside the file. A conversation can be one or many battle lines, displayed as small windows inside the program.
+
+The only think you have to edit in these lines is in the rightmost textarea of the windows (under "Edited"). That's the text that will be saved in the end.
+
+Use the "Recalculate" button in each window to check if the lines you're using are too long. (this will be changed once VWF is implemented)
+
+If you want to turn ASCII text to SJIS (for example, after pasting it from a text file), use the option under the Tools menu. This will convert all edited text inside the current conversation to SJIS.
+
+To save the changes, go to File -> Save As... option.
+
+
+OTHER STUFF
+------------
+
+A typical attack conversation has one "NO ACTION" line and one "ACTION" line. Lines labeled as "NO ACTION" are used when the mech is standing still, whereas "ACTION" lines are said in the middle of an attack (shooting, for example). Other lines are typically one "NO ACTION" line. Of course, numbers will vary.
+
+The program automatically tells you which character is saying which line. Keep in mind that, even though a file is dedicated to one specific pilot, the conversations can have several people speaking (which is why I call them "conversations" in the first place). For example, Gou's lines usually involve Shou berating him and Gai trying to calm them down.
+
+You can use the ID and portrait numbers to locate the portrait used during the dialogue if you extracted the SP files from bpilot.pak (using the SP Extractor). For example, if the ID is 123 and the portrait is 456, the BMP files showing the portrait will be p123456.sp_x.bmp (where x is a number from 1 to 4). If a pilot has several outfits (like Amuro), there will be lines repeated for each outfit set where the only difference is the portrait.
+
+* I'll use this chance to say that the Dragonar boys (Kaine, Tapp and Light) have errors in some of their portraits. During their introduction stage (4?), you can see them wearing their pilot suits occasionally during battle, even though they're supposed to be seen with their student clothes during the whole scenario. That's an error of the game. This tool doesn't alter portraits in any way (unless I messed up) and won't do it in the future, since there's still some uncertainty about when exactly does a battle line trigger.
+
+The Voice value is simply informative. Some lines don't have any voices, such as the ones for Kouji when he's unconscious or some for the Nubia cultists.
+
+And that's pretty much it. Have fun.
---
+++ src/srwgcbattlescripteditor/BattleLine.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package srwgcbattlescripteditor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class BattleLine {
+        int charID;
+        int portrait;
+        int voice;
+        int status;
+        String originalText;
+        String editText;
+        byte[] extra;
+
+        public BattleLine(){
+            charID = 0;
+            portrait = 0;
+            voice = 0;
+            status = 1;
+            originalText = "";
+            editText = "";
+            extra = null;
+        }
+
+        // Return the battle line as a byte array with the format used in the BIN files.
+        // Special endings for lines are not included, however, the last byte will always be 0a
+        // (used for carriage returns), which is the first byte of every line's ending.
+        // The external routine that calls this program will have to add the rest of the bytes
+        // that form the special endings (0a 00 for the end of a "conversation" or 00 for the end of the file).
+        // Likewise, the first byte that is used in the first battle line (also 0a) is not included here
+        // and it rests on the external routine to handle as well.
+        public byte[] getBytes(){
+            byte[] result = null;
+
+            // Determine the size of the byte arrays
+            byte[] beginning = new byte[24];    // Up to the character ID, before the extra part
+
+            byte[] char_section = new byte[4];
+
+            String[] lines = editText.split("\n");
+
+            byte[] separator = new byte[1];
+            separator[0] = 0x0a;
+
+            // The voice section might not be present
+            byte[] voice_section = null;
+            if (voice != -1)
+                voice_section = new byte[10];
+
+            // Write the data
+            beginning[0] = (byte) (0x81 & 0xff);
+            beginning[1] = (byte) (0x95 & 0xff);
+            beginning[2] = (byte) (0x82 & 0xff);
+            beginning[3] = (byte) (0x86 & 0xff);
+
+            beginning[6] = 4;
+
+            beginning[8] = (byte) (portrait & 0xff);
+            beginning[9] = (byte) ( (portrait >> 8) & 0xff);
+            beginning[10] = (byte) ( (portrait >> 16) & 0xff);
+            beginning[11] = (byte) ( (portrait >> 24) & 0xff);
+
+            beginning[20] = (byte) (status & 0xff);
+
+            char_section[0] = (byte) (0x81 & 0xff);
+            char_section[1] = (byte) (0x93 & 0xff);
+
+            char_section[2] = (byte) (charID & 0xff);
+            char_section[3] = (byte) ( (charID >> 8) & 0xff);
+
+            if (voice_section != null){
+                voice_section[0] = (byte) (0x81 & 0xff);
+                voice_section[1] = (byte) (0x95 & 0xff);
+                voice_section[2] = (byte) (0x82 & 0xff);
+                voice_section[3] = (byte) (0x96 & 0xff);
+
+                voice_section[4] = (byte) (voice & 0xff);
+                voice_section[5] = (byte) ( (voice >> 8) & 0xff);
+                voice_section[6] = (byte) ( (voice >> 16) & 0xff);
+                voice_section[7] = (byte) ( (voice >> 24) & 0xff);
+            }
+
+            // Join everything
+            ByteArrayOutputStream os = new ByteArrayOutputStream( );
+            try {
+                os.write(beginning);
+
+                if (extra != null)
+                    os.write(extra);
+
+                os.write(char_section);
+
+                if (voice_section != null)
+                    os.write(voice_section);
+
+                for (int i = 0; i < lines.length; i++){
+                    os.write(lines[i].getBytes("Shift-JIS"));
+                    os.write(separator);
+                }
+
+                result = os.toByteArray();
+            } catch (IOException ex) {
+                Logger.getLogger(BattleLine.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return result;
+        }
+}
