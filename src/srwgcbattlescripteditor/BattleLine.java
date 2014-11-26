3ef5a439ef194d7d4e6f0bd52ffdb39fbcb40e26
DashmanGC <ulricvonmesnar@gmail.com>
2014-11-26 14:06:34 +0100

    Version 1.2a
    License added
    
    Readme has been updated.
    
    The font encoding had been
    overlooked when writing to a file, generating
    errors in some characters
    (like "-"). Now it should work.

--- src/srwgcbattlescripteditor/BattleLine.java
+++ src/srwgcbattlescripteditor/BattleLine.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Dashman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package srwgcbattlescripteditor;
@@ -22,6 +34,8 @@ public class BattleLine {
         String originalText;
         String editText;
         byte[] extra;
+        
+        String font_encoding = "MS932";
 
         public BattleLine(){
             charID = 0;
@@ -105,7 +119,7 @@ public class BattleLine {
                     os.write(voice_section);
 
                 for (int i = 0; i < lines.length; i++){
-                    os.write(lines[i].getBytes("Shift-JIS"));
+                    os.write(lines[i].getBytes(font_encoding));
                     os.write(separator);
                 }
 
